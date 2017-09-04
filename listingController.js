e2f6f0cf37a7c548ef422202b3ff9212e8921975
zhangcandrew <zhangcandrew@gmail.com>
2017-09-04 19:20:07 -0400

    Changed the .js and .html file to make the App work as expected
    I made changes to the .js and .html using Angular to make our
    application work as we'd expect it to.

--- listingController.js
+++ listingController.js
@@ -7,8 +7,25 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push({'code': $scope.input.inputCode, 'name': $scope.input.inputName, 'coordinates': {'latitude': $scope.input.inputCoLa, 'longitude': $scope.input.inputCoLo}, 'address': $scope.input.inputAddr});
+      $scope.input="";
+    };
+
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice($scope.listings.indexOf(index), 1);
+
+    };
+
+    $scope.showDetails = function(index) {
+      var tempResults;
+      for(var i = 0; i<Listings.length; i++){
+        if(Listings[i].code == index){
+          tempResults = Listings[i];
+        }
+      }
+
+      $scope.detailedInfo = tempResults;
+    };
   }
 ]);
